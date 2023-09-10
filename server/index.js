2d690d0e0d09847c98c25740ae0fd81f36db81dd
KevSalom <kevsaloms@gmail.com>
2023-09-09 21:04:04 -0400

    some change

--- server/index.js
+++ server/index.js
@@ -1,34 +1,37 @@
+require("dotenv").config();
+const {
+  PORT
+} = process.env;
+
 const axios = require("axios");
 const server = require("./src/server");
-const { conn, Activity, Country } = require('./src/db.js');
-const PORT = 3001;
-
+const { conn, Country } = require('./src/db.js');
 
 async function startServer (){
 
   try {
-    await conn.sync({ alter:true });
+    await conn.sync({ force:true });
     server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   })
-  // const {data} = await axios.get('http://localhost:5000/countries');
-  // const countries = data.map((c) => {
-  //  return c = {
-  //     id : c.cca3,
-  //     name: c.translations.spa.common,
-  //     capital: c.capital,
-  //     continent: c.continents[0],
-  //     subregion: c.subregion,
-  //     area: c.area,
-  //     population: c.population,
-  //     languages: c.languages && Object.values(c.languages),
-  //     currencies: c.currencies,
-  //     timezone: c.timezones[0],
-  //     flag: c.flags.svg,
-  //     map: c.maps.googleMaps
-  //   }})
+  const {data} = await axios.get('http://localhost:5000/countries');
+  const countries = data.map((c) => {
+   return c = {
+      id : c.cca3,
+      name: c.translations.spa.common,
+      capital: c.capital,
+      continent: c.continents[0],
+      subregion: c.subregion,
+      area: c.area,
+      population: c.population,
+      languages: c.languages && Object.values(c.languages),
+      currencies: c.currencies,
+      timezone: c.timezones[0],
+      flag: c.flags.svg,
+      map: c.maps.googleMaps
+    }})
 
-  //   await Country.bulkCreate(countries);
+    await Country.bulkCreate(countries);
   } catch (error) {
     console.log (error.message)
   }
