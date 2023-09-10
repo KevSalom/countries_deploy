d4614ab555ce61b44ca563f8a14d55c5e0d2ee7c
KevSalom <kevsaloms@gmail.com>
2023-09-09 23:14:52 -0400

    get countris

--- server/index.js
+++ server/index.js
@@ -14,24 +14,24 @@ async function startServer (){
     server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   })
-  // const {data} = await axios.get('http://localhost:5000/countries');
-  // const countries = data.map((c) => {
-  //  return c = {
-  //     id : c.cca3,
-  //     name: c.translations.spa.common,
-  //     capital: c.capital,
-  //     continent: c.continents[0],
-  //     subregion: c.subregion,
-  //     area: c.area,
-  //     population: c.population,
-  //     languages: c.languages && Object.values(c.languages),
-  //     currencies: c.currencies,
-  //     timezone: c.timezones[0],
-  //     flag: c.flags.svg,
-  //     map: c.maps.googleMaps
-  //   }})
+  const {data} = await axios.get('http://localhost:5000/countries');
+  const countries = data.map((c) => {
+   return c = {
+      id : c.cca3,
+      name: c.translations.spa.common,
+      capital: c.capital,
+      continent: c.continents[0],
+      subregion: c.subregion,
+      area: c.area,
+      population: c.population,
+      languages: c.languages && Object.values(c.languages),
+      currencies: c.currencies,
+      timezone: c.timezones[0],
+      flag: c.flags.svg,
+      map: c.maps.googleMaps
+    }})
 
-  //   await Country.bulkCreate(countries);
+    await Country.bulkCreate(countries);
   } catch (error) {
     console.log (error.message)
   }
